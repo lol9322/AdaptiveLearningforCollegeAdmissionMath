377c56b743e508a97420c3b55f49753877ebcea6
MilyangParkJaeHoon <jh9277@naver.com>
2018-07-31 21:47:27 +0900

    finish_first_clustering_v1

--- algorithm/clustering/demo_clustering.py
+++ algorithm/clustering/demo_clustering.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import tensorflow as tf
-
-init_data = [[0.9,0.3,0.8,0.2],[0.5,0.4,0.7,0.8],[0.1,0.2,0.3,0.4]]
-print(len(init_data))
+import random
+np.random.seed(1)
+init_data = np.random.rand(2,500)
 data = {"x": [], "y": [], "cluster": [], "student": []}
-k = int(math.sqrt(len(init_data[0])))
+k = 9
 for now in range(len(init_data)):
+    temp = 0
     vectors_set = init_data[now]
     vectors = tf.constant(vectors_set)
     centroides = tf.Variable(tf.slice(tf.nn.top_k(tf.random_shuffle(vectors),len(vectors_set),True)[0],[0],[k]))
@@ -20,7 +21,7 @@ for now in range(len(init_data)):
     diff = tf.sub(expanded_vectors, expanded_centroides)
     sqr = tf.square(diff) # 제곱
     assignments = tf.argmin(sqr,0) # 최소값 인덱스
-
+    # print(sess.run(assignments))
     # # 업데이트 : 새로운 중심 계산
     means = tf.concat(0,
                     [tf.reduce_mean(
@@ -30,13 +31,27 @@ for now in range(len(init_data)):
                                         ,[1, -1])
                                     )
                         , reduction_indices=[1]) for c in range(k)])
+
     update_centroides = tf.assign(centroides, means)
+    past = tf.reduce_sum(tf.square(tf.sub(centroides,temp)))
+    temp = centroides
+
     init_op = tf.initialize_all_variables()
     sess = tf.Session()
     sess.run(init_op)
-    for step in range(400):
-        sess.run(update_centroides)
-        assignment_values = sess.run(assignments)
+    count = 0
+    for step in range(50):
+        count+=1
+        a = sess.run(temp)
+        b = sess.run(update_centroides)
+        gap = 0
+        for i in range(len(a)):
+            gap += (a[i]-b[i])**2
+        gap /= len(a)
+        if(gap<0.00000000000005):
+            break
+    assignment_values = sess.run(assignments)
+    print(count)
     for i in range(len(assignment_values)):
         data["x"].append(vectors_set[i])
         data["student"].append(i)
@@ -48,11 +63,16 @@ sns.lmplot("x","y", data=df, fit_reg=False, size=6, hue="cluster", legend=False)
 plt.show()
 
 student = data["student"]
-print(student)
 unit = data["y"]
-print(unit)
 cluster  = data["cluster"]
-matrix = [[[]]*k for i in range(len(init_data))]
-for i in range(len(student)):
+matrix = [[[] for j in range(k)] for i in range(len(init_data))]
+for i in range(len(init_data)*len(init_data[0])):
     matrix[unit[i]][cluster[i]].append(student[i])
 print(matrix)
+
+# tf.concat(concat_dim,values)
+# :텐서들을 하나의 차원에서 이어붙인다.
+# t1 = [[1, 2, 3], [4, 5, 6]]
+# t2 = [[7, 8, 9], [10, 11, 12]]
+# tf.concat(0, [t1, t2]) ==> [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+# tf.concat(1, [t1, t2]) ==> [[1, 2, 3, 7, 8, 9], [4, 5, 6, 10, 11, 12]]
